fb92109e85e34e6b38bd2a6607dbb7a1862292c7
Antoine Mansour <129776972+antoinemansour7@users.noreply.github.com>
2024-10-18 14:14:07 -0400

    Fixes #8: Added display error messages if user tries to create account with already used email/username

--- forms.py
+++ forms.py
@@ -9,21 +9,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-# Registration Form Definition
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=150)])
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=1, max=120)])  # Add age field
+    age = IntegerField('Age', validators=[DataRequired(), NumberRange(min=1, max=120)])
     role = SelectField('Role', choices=[('customer', 'Customer'), ('instructor', 'Instructor')], validators=[DataRequired()])
     submit = SubmitField('Sign Up')
 
+    # Validator to check if username already exists
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is already taken. Please choose a different one.')
 
+    # Validator to check if email already exists
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
